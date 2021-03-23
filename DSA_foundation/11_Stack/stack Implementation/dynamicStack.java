03ef74191c17fa3b3c2a862e1d0445d2e1e2d592
rishabhsinghal1007 <rishabhsinghal1007@gmail.com>
2021-03-23 14:09:37 +0530

    update

--- DSA_foundation/11_Stack/stack Implementation/dynamicStack.java
+++ DSA_foundation/11_Stack/stack Implementation/dynamicStack.java
@@ -2,24 +2,20 @@ public class dynamicStack extends stackClass{
     
     public dynamicStack(){
         super();
-        // this.capacity = 10;
-        // initializeVariables();
     } 
 
     public dynamicStack(int size){
         super(size);
-        // this.capacity = size;
-        // super.initializeVariables();
     }
-    // array provide kray aur array convert into stack ...
+
     public dynamicStack(int[] arr){
         super.capacity = arr.length;
-        super.initializeVariables(arr.length);
+        super.initializeVariables(2 * arr.length);
 
         for(int ele : arr)
             super.push_(ele);
     }
-
+        // new function 
     // protected void push_(int data){
     //     if(super.capacity == super.elementCount){ // check my stack is overflow or not
     //         int[] temp = super.arr; // copy all data
@@ -28,7 +24,7 @@ public class dynamicStack extends stackClass{
     //             super.push_(ele);
     //     }
 
-    //     super.push_(data);   // add data jiski vjah se size overflow hua tha
+    //     super.push_(data);   
     // }
 
     // public void push(int data){
---
+++ DSA_foundation/12_QUEUE/queue Implementation/dynamicQueue.java
@@ -0,0 +1,33 @@
+public class dynamicQueue extends queueClass{
+
+    public dynamicQueue(){
+        super();
+    }
+
+    public dynamicQueue(int size){
+        super(size);
+    }
+   
+    public dynamicQueue(int[] arr){
+        super(2 * arr.length);
+        for(int ele : arr){
+            super.push_(ele);
+        }
+    }
+
+    @Override
+    public void push(int data) throws Exception{
+        if(super.capacity == super.elementCount){
+            int[] temp = new int[super.capacity];
+            for(int i = 0 ; i < super.capacity ; i++){
+                int idx = (super.front + i) % super.capacity;
+                temp[i] = super.arr[idx];
+            }
+
+            super.initializeVariables(2 * super.capacity);
+            for (int ele : temp)
+            super.push_(ele);
+        }
+        super.push_(data);
+    }
+}
