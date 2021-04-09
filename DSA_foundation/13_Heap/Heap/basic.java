15fd9e401bcb3ee2aa06200854d3ceed478ee6f4
rishabhsinghal1007 <rishabhsinghal1007@gmail.com>
2021-04-09 23:16:30 +0530

    update

--- DSA_foundation/13_Heap/Heap/basic.java
+++ DSA_foundation/13_Heap/Heap/basic.java
@@ -20,8 +20,6 @@ public class basic {
         return ls + rs + 1;
     }
 
-
-
     public static void main(String[] args) {
         int[] arr = { 10, 20, 30, -2, -3, -4, 5, 6, 7, 8, 9, 22, 11, 13, 14 };
         System.out.println(height(arr, 0));
