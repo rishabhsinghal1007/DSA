ae8dd1f84f5b177b5530a3b75f02c06dc51ab328
rishabhsinghal1007 <rishabhsinghal1007@gmail.com>
2021-07-06 23:09:55 +0530

    commit

---
+++ DSA_foundation/14_Graph/l001Basic.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+
+public class l001Basic {
+
+    public static class Edge {
+        int v = 0;
+        int w = 0;
+
+        Edge(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+
+        public String toString() {
+            return "(" + this.v + ", " + this.w + ") ";
+        }
+    }
+
+    public static int N = 7;
+    public static ArrayList<Edge>[] graph = new ArrayList[N];
+
+    public static void addEdge(int u, int v, int w) {
+        graph[u].add(new Edge(v, w));
+        graph[v].add(new Edge(u, w));
+    }
+
+    public static void display() {
+        for (int i = 0; i < N; i++) {// hr edge pr traverse kra
+            System.out.print(i + " -> ");
+
+            for (Edge e : graph[i]) { // graph[i] gives u ArrayList of Edge which contains u & w
+                System.out.print(e);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int findEdge(int u, int v) {
+        for (int i = 0; i < graph[u].size(); i++) {
+            Edge e = graph[u].get(i);
+            if (e.v == v) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void removeEdge(int u, int v) {
+        int idx1 = findEdge(u, v);
+        int idx2 = findEdge(v, u);
+
+        // if (idx1 == -1 || idx2 == -2) {
+        // return;
+        // }
+
+        graph[u].remove(idx1);
+        graph[v].remove(idx2);
+
+    }
+
+    public static void removeVtx(int u) {
+        int n = graph[u].size();
+        while (n != 0) {
+            Edge e = graph[u].get(n - 1);
+            removeEdge(u, e.v);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < N; i++) {
+            graph[i] = new ArrayList<>();
+
+        }
+        addEdge(0, 1, 10);
+        addEdge(0, 3, 10);
+        addEdge(1, 2, 10);
+        addEdge(2, 3, 10);
+        addEdge(3, 4, 2);
+        addEdge(4, 5, 3);
+        addEdge(4, 6, 8);
+        addEdge(5, 6, 3);
+
+        display();
+    }
+}
